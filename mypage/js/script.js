fda2a9c0779858ccbe478ff54107aafea13d573e
Lorianne Gilmore <L.GILMORE3@students.clark.edu>
2021-05-08 19:12:45 -0700

    Framework

--- mypage/js/script.js
+++ mypage/js/script.js
@@ -10,7 +10,7 @@ let value = 0
 // trail index number
 let trailValue = 0
 // interval (Duration)
-let interval = 4000
+let interval = 40000
 
 // Function to slide forward
 const slide = (condition) => {
